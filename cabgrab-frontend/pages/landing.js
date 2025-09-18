cc4da6be4c4c0c1f942f1c4299eb6c8e3aed1711
Ethan Seiz <ezs8@cornell.edu>
2025-09-18 11:57:31 -0400

    cleaning up

--- cabgrab-frontend/pages/landing.js
+++ cabgrab-frontend/pages/landing.js
@@ -7,7 +7,6 @@ export default function Landing() {
     <>
       <Head>
         <title>CABgrab - Login</title>
-        {/* <link rel="stylesheet" href="/css/style.css" /> */}
       </Head>
       <body className="landing-page">
         <div className="landing-container">
@@ -18,7 +17,6 @@ export default function Landing() {
             Track Brown University course availability in real-time.
           </p>
 
-          {/* For now, link to login API directly */}
           <a href="/api/login" className="login-btn">
             <img
               src="/static/logos/icons8-google-48.png"
